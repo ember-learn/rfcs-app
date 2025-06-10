1ed15d3f43fa7436fbd0ac9a40460dde0874f84f
Chris Manson <chris@manson.ie>
2025-06-10 15:44:16 +0100

    remove deprecation workflow

--- app/app.js
+++ app/app.js
@@ -3,12 +3,7 @@ import compatModules from '@embroider/virtual/compat-modules';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'rfcs-app/config/environment';
-import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import './app-styles/app.css';
-
-if (macroCondition(isDevelopingApp())) {
-  importSync('./deprecation-workflow');
-}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
