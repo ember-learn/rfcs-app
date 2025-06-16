664ee79f337874bef44a13cf06cc40de70fcce79
Chris Manson <chris@manson.ie>
2025-06-16 16:36:32 +0100

    fix case sensitive names

--- app/routes/rfc.js
+++ app/routes/rfc.js
@@ -1,9 +1,16 @@
 import Route from '@ember/routing/route';
 
+const rfcs = import.meta.glob('rfcs/text/*.md');
+
 export default class RfcsRoute extends Route {
   async model(params) {
-    const rfcId = params.id;
-    const rfc = (await import(`rfcs/text/${rfcId}.md`)).default
+    /**
+     * we need to normalise the ID here because Netlify by default forces all URLs to be lowercase, that means that
+     * there starts to be a discrepency between what vite can see on disk and the actual id that we're trying to access
+     */
+    const rormalisedRFCId = Object.keys(rfcs).find(id => id.toLowerCase() === `/rfcs/text/${params.id.toLowerCase()}.md`)
+
+    const rfc = (await rfcs[rormalisedRFCId]()).default;
 
     return {
       ...rfc,
