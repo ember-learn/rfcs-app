0c91258264d010a7c676cfd024b5c7ebf21ddf0f
Chris Manson <chris@manson.ie>
2025-07-31 17:35:08 +0100

    add data scripts

---
+++ combineData.mjs
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+
+import { readdir, readFile, writeFile } from "fs/promises";
+import { join } from "path";
+
+const rfcsFiles = await readdir("./data");
+
+rfcsFiles.sort((a, b) => parseInt(a) - parseInt(b));
+
+let rfcMap = {};
+
+function getDays(date1, date2) {
+  if (date1) {
+    return Math.ceil(
+      (new Date(date1) - new Date(date2)) / (1000 * 60 * 60 * 24),
+    );
+  } else {
+    return Math.ceil((new Date() - new Date(date2)) / (1000 * 60 * 60 * 24));
+  }
+}
+
+function findFirstStage(rfc) {
+  if (rfc.merged) {
+    return "accepted";
+  } else if (!rfc.closed) {
+    if (
+      rfc.timelineItems.filter((item) => item.label == "S-Exploring").length ==
+      1
+    ) {
+      return "exploring";
+    } else {
+      return "proposed";
+    }
+  } else {
+    return "closed";
+  }
+}
+
+function getFirstStagesDuration(rfc) {
+  // console.log(rfc);
+  if (!rfc.closed || rfc.merged) {
+    let exploringLabels = rfc.timelineItems.filter(
+      (item) => item.label == "S-Exploring",
+    );
+    if (exploringLabels.length > 0) {
+      if (rfc.merged) {
+        return {
+          proposed: getDays(exploringLabels[0].createdAt, rfc.createdAt),
+          exploring: getDays(rfc.mergedAt, exploringLabels[0].createdAt),
+          accepted: null,
+          release: null,
+          released: null,
+        };
+      } else {
+        return {
+          proposed: getDays(exploringLabels[0].createdAt, rfc.createdAt),
+          exploring: getDays(new Date(), exploringLabels[0].createdAt),
+          accepted: null,
+          release: null,
+          released: null,
+        };
+      }
+    } else {
+      return {
+        proposed: getDays(new Date(), rfc.createdAt),
+        exploring: null,
+        accepted: null,
+        release: null,
+        released: null,
+      };
+    }
+  } else {
+    return {
+      closed: this.getDays(rfc.closedAt, rfc.createdAt),
+    };
+  }
+}
+
+for (let file of rfcsFiles) {
+  const rfc = JSON.parse(await readFile(join("data", file), "utf8"));
+
+  if (rfc.title.includes("Advance RFC")) {
+    let advanceRFC = rfc.title.match(/[0-9]+/);
+    let num = parseInt(advanceRFC, 10);
+    let stage = rfc.title.split(" ").pop().toLowerCase();
+    rfcMap[num].stageDuration[`${rfcMap[num].currentStage}`] = getDays(
+      rfc.mergedAt,
+      rfc.createdAt,
+    );
+    if (rfc.merged) {
+      rfcMap[num].currentStage = stage;
+    }
+    if (rfc.assignees.length > 0) {
+      rfcMap[num].assignees.push(...rfc.assignees);
+    }
+    rfcMap[num].connected.push(rfc);
+  } else {
+    rfcMap[rfc.number] = {
+      ...rfc,
+      currentStage: findFirstStage(rfc),
+      labels: rfc.timelineItems.filter((item) => item.label == "S-Exploring"),
+      stageDuration: getFirstStagesDuration(rfc),
+      connected: [],
+      assignees: rfc.assignees,
+    };
+  }
+}
+
+for (let rfc in rfcMap) {
+  console.log(rfc);
+  await writeFile(
+    join("rfc-data", `${rfc}.json`),
+    JSON.stringify(rfcMap[rfc], null, 2),
+  );
+}
---
+++ getData.mjs
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+
+import { Octokit } from "octokit";
+import { writeFile } from "node:fs/promises";
+import { join } from "path";
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const iterator = octokit.paginate.iterator(octokit.rest.pulls.list, {
+  owner: "emberjs",
+  repo: "rfcs",
+  state: "all",
+  per_page: 100,
+});
+
+for await (const { data: pulls } of iterator) {
+  for (const pull of pulls) {
+    let { data: issueEvents } = await octokit.rest.issues.listEvents({
+      owner: "emberjs",
+      repo: "rfcs",
+      issue_number: pull.number,
+    });
+
+    issueEvents = issueEvents.filter((item) =>
+      ["labeled", "unlabeled"].includes(item.event),
+    );
+
+    await writeFile(
+      join("data", `${pull.number}.json`),
+      JSON.stringify(
+        {
+          number: pull.number,
+          url: pull.url,
+          title: pull.title,
+          createdAt: pull.created_at,
+          closed: pull.closed,
+          closedAt: pull.closed_at,
+          merged: pull.merged,
+          mergedAt: pull.merged_at,
+          assignees: pull.assignees.map((item) => ({
+            login: item.login,
+            avatarUrl: item.avatar_url,
+          })),
+          timelineItems: issueEvents.map(item => ({
+            event: item.event,
+            createdAt: item.created_at,
+            label: item.label.name,
+          })),
+        },
+        null,
+        2,
+      ),
+      "utf8",
+    );
+  }
+}
+
+// const pulls = await octokit.rest.pulls.list({
+//   owner: 'emberjs',
+//   repo: 'ember.js'
+// })
+
+// console.log(pulls);
+
+// const result = await octokit.graphql.paginate(
+//   `
+//     query ($endCursor:String) {
+//       repository(owner: "EmberJS", name: "rfcs") {
+//         pullRequests(after: $endCursor, first: 100, orderBy:{ field: CREATED_AT, direction: DESC} ) {
+//           pageInfo {
+//             hasNextPage
+//             endCursor
+//           }
+//           nodes {
+//             number
+//             url
+//             title
+//             createdAt
+//             closed
+//             closedAt
+//             merged
+//             mergedAt
+//             assignees(first: 5) {
+//               nodes {
+//                 login
+//                 avatarUrl
+//               }
+//             }
+//             timelineItems(first: 10, itemTypes: [LABELED_EVENT, UNLABELED_EVENT]) {
+//               nodes {
+//                 ... on LabeledEvent {
+//                   createdAt
+//                   label {
+//                     name
+//                   }
+//                   __typename
+//                 }
+//                 ... on UnlabeledEvent {
+//                   createdAt
+//                   label {
+//                     name
+//                   }
+//                   __typename
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `,
+// );
+
+// console.log(result)
