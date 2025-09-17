eda92bf86e20dd307244c4e343e72f24ca22ded1
Chris Manson <chris@manson.ie>
2025-09-17 12:23:04 +0200

    fix ready-for-release route

--- app/routes/stages/ready-for-release.js
+++ app/routes/stages/ready-for-release.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class StagesReadyForReleaseRoute extends Route {
   async model() {
     const result = (
-      await import('rfcs-app-toc-builder:stage-release.json')
+      await import('rfcs-app-toc-builder:stage-ready-for-release.json')
     ).default;
     return result;
   }
