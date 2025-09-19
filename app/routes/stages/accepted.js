330b33002949783874bd160a87086ee962d78913
Chris Manson <chris@manson.ie>
2025-09-19 10:43:37 +0200

    filter non-rfcs in accepted

--- app/routes/stages/accepted.js
+++ app/routes/stages/accepted.js
@@ -4,6 +4,11 @@ export default class StagesAcceptedRoute extends Route {
   async model() {
     const result = (await import('rfcs-app-toc-builder:stage-accepted.json'))
       .default;
-    return result;
+
+    /**
+     * filter out PRs that don't have RFC files - i.e. are not RFCS
+     * TODO figuire out the combine logic so that we don't get the data in here at this level
+     */
+    return result.filter((item) => Boolean(item.rfcFile));
   }
 }
