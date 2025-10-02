2bcfa44186289e027c384267feed935557f0a95a
Chris Manson <chris@manson.ie>
2025-10-02 13:04:05 +0100

    add inspector support

--- app/app.js
+++ app/app.js
@@ -5,10 +5,14 @@ import loadInitializers from 'ember-load-initializers';
 import config from 'rfcs-app/config/environment';
 import 'ember-a11y-refocus/styles.css';
 
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
+
+  inspector = setupInspector(this);
 }
 
 loadInitializers(App, config.modulePrefix, compatModules);
